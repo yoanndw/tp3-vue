4d433bd1cb925e83c0157c32fa20b5aed191398c
Yoann DEWILDE <yoanndewilde@gmail.com>
2023-03-24 12:09:46 +0100

    Add toggle button and focus and background directives

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router).mount('#app')
+
+const backgroundDirective = {
+  mounted (el, binding) {
+    el.style.backgroundColor = binding.arg || 'yellow'
+  }
+}
+
+app.directive('focus', {
+  mounted: el => el.focus()
+})
+app.directive('background', backgroundDirective)
