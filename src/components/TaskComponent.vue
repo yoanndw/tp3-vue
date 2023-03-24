3a67cbffe8358e3c4d327de03a86801baaecefb2
Yoann DEWILDE <yoanndewilde@gmail.com>
2023-03-24 12:43:12 +0100

    Create basic task and todos components

---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <input type="checkbox" />
+    <p>{{ title }}</p>
+  </li>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  props: {
+    title: String
+  }
+})
+export default class TaskComponent extends Vue {
+  title!: string;
+}
+</script>
+
+<style></style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <ul v-for="t in tasks"> -->
+    <TaskComponent title="Code" />
+  <!-- </ul> -->
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import TaskComponent from '@/components/TaskComponent.vue'
+
+@Options({
+  components: {
+    TaskComponent
+  }
+})
+export default class Todos extends Vue {
+  // tasks: 
+}
+</script>
+
+<style scoped>
+</style>
