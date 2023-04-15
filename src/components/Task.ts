5840ad00337b9e7864a0a06ad2791d83bd49371c
Yoann DEWILDE <yoanndewilde@gmail.com>
2023-04-15 13:07:36 +0200

    Improve Task class

--- src/components/Task.ts
+++ src/components/Task.ts
@@ -1,5 +1,11 @@
 /* eslint-disable no-useless-constructor */
 export default class Task {
   done = false;
-  constructor (public title: string) {}
+
+  constructor (public title: string) {
+  }
+
+  public toggle() {
+    this.done = !this.done;
+  }
 }
